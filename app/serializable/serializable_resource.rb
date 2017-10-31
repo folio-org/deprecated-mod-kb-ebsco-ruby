503529ec9642daf2e339950ef82dfe55e9e7e395
Wil Wilsman <wil@frontside.io>
2017-10-31 16:16:46 -0500

    Use camelcase keys for json-api resources

--- app/serializable/serializable_resource.rb
+++ app/serializable/serializable_resource.rb
@@ -1,5 +1,5 @@
 class SerializableResource < JSONAPI::Serializable::Resource
   extend JSONAPI::Serializable::Resource::KeyFormat
 
-  key_format -> (key) { key.to_s.dasherize }
+  key_format -> (key) { key.to_s.camelize :lower }
 end
