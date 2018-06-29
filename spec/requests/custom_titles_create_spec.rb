eddcb20618cdf27a339dde1352c84a6dfb7658e3
Charles Lowell <cowboyd@frontside.io>
2018-06-28 22:09:16 -0600

    Remove errant `p`

--- spec/requests/custom_titles_create_spec.rb
+++ spec/requests/custom_titles_create_spec.rb
@@ -44,7 +44,6 @@ RSpec.describe 'Custom Titles Create', type: :request do
     end
 
     it 'responds with OK status' do
-      p response.body
       expect(response).to have_http_status(200)
     end
 
