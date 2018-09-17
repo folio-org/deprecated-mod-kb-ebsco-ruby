3d0c7232e6beafb2d83d6e3b30c2a2130f0d2c24
carolegodfrey <cgodfrey@ebsco.com>
2018-09-17 16:40:00 -0400

    Add default handling from rm api base url when not part of post

--- spec/requests/configurations_spec.rb
+++ spec/requests/configurations_spec.rb
@@ -39,6 +39,23 @@ RSpec.describe 'Configurations', type: :request do
     ]
   end
 
+  let(:resource_with_missing_url) do
+    [
+      '/eholdings/configuration',
+      params: {
+        data: {
+          type: 'configurations',
+          id: 'default',
+          attributes: {
+            customerId: customer_id,
+            apiKey: api_key
+          }
+        }
+      }.to_json,
+      headers: headers
+    ]
+  end
+
   let(:resource_with_invalid_content_type_header) do
     [
       '/eholdings/configuration',
@@ -104,6 +121,38 @@ RSpec.describe 'Configurations', type: :request do
       end
 
       it 'contains valid attributes' do
+        expect(json.data.attributes.customerId).to eql(customer_id)
+        expect(json.data.attributes.apiKey).to eql(masked_api_key)
+        expect(json.data.attributes.rmapiBaseUrl).to eql(rmapi_url)
+      end
+    end
+  end
+
+  describe 'setting the configuration with missing url' do
+    before do
+      VCR.use_cassette('put-configuration-missing-url') do
+        put(*resource_with_missing_url)
+      end
+    end
+
+    let!(:json) { Map JSON.parse response.body }
+
+    it 'expects the response to have 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    describe 'reading the configuration' do
+      before do
+        VCR.use_cassette('get-configuration-missinurl') do
+          get(*resource)
+        end
+      end
+
+      it 'expect the response to be 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'updates configuration and defaults to currently configured url' do
         expect(json.data.attributes.customerId).to eql(customer_id)
         expect(json.data.attributes.apiKey).to eql(masked_api_key)
         expect(json.data.attributes.rmapiBaseUrl).to eql(rmapi_url)
